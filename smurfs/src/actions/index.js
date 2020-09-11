292f9228b9a770b7dff9d1863c70f71b64d97b00
Sara <scearc930@gmail.com>
2020-09-11 12:02:52 -0400

    got smurfs to display to page

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export const FETCH_SMURFS = "FETCH_SMURFS";
+export const FETCH_SMURFS_SUCCESS = "FETCH_SMURFS_SUCCESS";
+export const FETCH_SMURFS_ERROR = "FETCH_SMURFS_ERROR";
+
+export const fetchSmurfs = () => {
+    return(dispatch) => {
+        dispatch({type: FETCH_SMURFS});
+        axios.get('http://localhost:3333/smurfs')
+        .then((res) => {
+            console.log(res.data)
+            dispatch({type: FETCH_SMURFS_SUCCESS,
+            payload: res.data})
+        })
+        .catch((err) => {
+            dispatch({type: FETCH_SMURFS_ERROR,
+            payload: {message: "Whoops! Looks like Gargamel got here first!"}})
+        })
+    }
+}
---
+++ smurfs/src/components/smurf.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Smurf = ({smurf}) => {
+
+    return <div className='smurf-card'>
+        <h2>{smurf.name}</h2>
+    </div>
+}
+
+export default Smurf
